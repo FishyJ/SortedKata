12a043a35ec44beec1209140365070482983a01b
Jason Haughton <jason.haughton@gmail.com>
2020-03-08 21:26:55 +0000

    2nd test passing

--- SortedKata/CheckoutTests.cs
+++ SortedKata/CheckoutTests.cs
@@ -24,5 +24,29 @@ namespace CheckoutKataTest
             //Assert
             Assert.AreEqual(0m, total);
         }
+
+        [Test]
+        public void WhereCheckoutIsSimpleSumOfItems()
+        {
+            //Arrange
+            List<Item> items = new List<Item>();
+            items.Add(new Item("A99", 0.50m, "Apple"));
+            items.Add(new Item("B15", 0.30m, "Pack of biscuits"));
+            items.Add(new Item("C40", 0.60m, "Something else"));
+
+            CheckoutKata.Repository repo = new RepositoryForTesting(items);
+            Checkout checkout = new Checkout(repo);
+
+            checkout.Scan(repo.GetItem("A99"));
+            checkout.Scan(repo.GetItem("B15"));
+            checkout.Scan(repo.GetItem("C40"));
+
+
+            //Act
+            decimal total = checkout.Total();
+
+            //Assert
+            Assert.AreEqual(1.40m, total);
+        }
     }
 }
